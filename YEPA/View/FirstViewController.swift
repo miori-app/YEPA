a8539319babb04e1a7c2f4999008d15d0ea6c12b
iammiori <mioriii@naver.com>
2021-07-26 23:54:57 +0900

    [feat] 로그인 화면 - 이미지, 라벨 추가

--- YEPA/View/FirstViewController.swift
+++ YEPA/View/FirstViewController.swift
@@ -10,13 +10,54 @@ import SnapKit
 import Then
 
 class FirstViewController: UIViewController {
+    
+    let mainLogo = UIImageView().then {
+        let tmpImg = UIImage(named: "TextLogo.png")
+        // image Resize
+        //$0.image = tmpImg?.aspectFittedToHeight(45)
+        $0.image = UIImage(named: "YEPAO.png")
+        $0.contentMode = .scaleAspectFit
+        //$0.clipsToBounds = false
+    }
+    
+    
+
+    
+    let boldLabel = UILabel().then {
+        $0.numberOfLines = 0
+        $0.text = "Your English Partner"
+        $0.textColor = .black
+        $0.font = UIFont.boldSystemFont(ofSize: 21)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
-        print("code based UI setting finished")
+        self.view.backgroundColor = .white
+        
+        _ = [mainLogo,boldLabel].map { self.view.addSubview($0)}
+        
+        bindConstraints()
     }
+    
+    
 
+}
 
+extension FirstViewController {
+    
+    private func bindConstraints(){
+        mainLogo.snp.makeConstraints {
+            $0.centerY.equalToSuperview().offset(-30)
+            $0.centerX.equalToSuperview()
+        }
+        
+        
+        boldLabel.snp.makeConstraints {
+            $0.top.equalTo(mainLogo.snp.bottom).offset(59.14)
+            $0.centerX.equalToSuperview()
+        }
+    }
 }
 
