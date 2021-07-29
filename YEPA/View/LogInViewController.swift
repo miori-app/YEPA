e89f42ec4e311e74d9a375b81e65a375ef825057
iammiori <mioriii@naver.com>
2021-07-29 23:45:39 +0900

    [feat] 버튼제외 로그인 UI 구성

--- YEPA/View/LogInViewController.swift
+++ YEPA/View/LogInViewController.swift
@@ -12,7 +12,7 @@ import Then
 class LogInViewController: UIViewController {
     
     let mainLogo = UIImageView().then {
-        let tmpImg = UIImage(named: "TextLogo.png")
+        //let tmpImg = UIImage(named: "TextLogo.png")
         // image Resize
         //$0.image = tmpImg?.aspectFittedToHeight(45)
         $0.image = UIImage(named: "YEPAO.png")
@@ -20,14 +20,23 @@ class LogInViewController: UIViewController {
         //$0.clipsToBounds = false
     }
     
-    
-
-    
+    let mainOrangeLogo = UIImageView().then {
+        $0.image = UIImage(named: "YEPASO.png")
+        $0.contentMode = .scaleAspectFit
+    }
+ 
     let boldLabel = UILabel().then {
         $0.numberOfLines = 0
         $0.text = "Your English Partner"
-        $0.textColor = .black
+        $0.textColor = .dG2
         $0.font = UIFont.boldSystemFont(ofSize: 21)
+    }
+    
+    let subLabel = UILabel().then {
+        $0.numberOfLines = 0
+        $0.text = "영어능력이 검증된 파트너들이 여러분의 글을\n매력적으로 번역해드립니다"
+        $0.textColor = .gray3
+        $0.textAlignment = .center
     }
 
     override func viewDidLoad() {
@@ -36,7 +45,7 @@ class LogInViewController: UIViewController {
         // Do any additional setup after loading the view.
         self.view.backgroundColor = .white
         
-        _ = [mainLogo,boldLabel].map { self.view.addSubview($0)}
+        _ = [mainLogo,mainOrangeLogo,boldLabel,subLabel].map { self.view.addSubview($0)}
         
         bindConstraints()
     }
@@ -53,9 +62,19 @@ extension LogInViewController {
             $0.centerX.equalToSuperview()
         }
         
+        mainOrangeLogo.snp.makeConstraints {
+            $0.bottom.equalTo(mainLogo.snp.top).offset(-103)
+            $0.centerX.equalToSuperview()
+            $0.height.equalTo(59.07)
+        }
         
         boldLabel.snp.makeConstraints {
             $0.top.equalTo(mainLogo.snp.bottom).offset(59.14)
+            $0.centerX.equalToSuperview()
+        }
+        
+        subLabel.snp.makeConstraints {
+            $0.top.equalTo(boldLabel.snp.bottom).offset(16)
             $0.centerX.equalToSuperview()
         }
     }
